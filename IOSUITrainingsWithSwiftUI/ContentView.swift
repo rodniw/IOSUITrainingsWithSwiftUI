7fe5a5558a6b489f6078de59b47513226f791fbb
Rodion <rodion.prosvirnin@bk.ru>
2020-07-26 22:27:32 +0300

    item for a recycler and recycler by foreach construction creation.

--- IOSUITrainingsWithSwiftUI/ContentView.swift
+++ IOSUITrainingsWithSwiftUI/ContentView.swift
@@ -23,7 +23,14 @@ struct ContentView: View {
             .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
             .background(Color.white.shadow(color: Color.black.opacity(0.18), radius: 5, x: 0, y: 5))
             
-            Spacer()
+            ScrollView {
+                VStack(spacing: 15) {
+                    //sets name as id
+                    ForEach(albums, id: \.album_name) { album in
+                        AlbumView(album: album)
+                    }
+                }
+            }
         }
         .background(Color.black.opacity(0.06).edgesIgnoringSafeArea(.all))
         .edgesIgnoringSafeArea(.top)
@@ -35,3 +42,31 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Data for the training
+struct Album{
+
+    var album_name : String
+    var album_author : String
+    var album_cover : String
+}
+
+var albums = [
+
+    Album(album_name: "Let Her Go", album_author: "Passenger", album_cover: "p1"),
+    Album(album_name: "Bad Blood", album_author: "Taylor Swift", album_cover: "p2"),
+    Album(album_name: "Believer", album_author: "Kurt Hugo Schneider", album_cover: "p3"),
+    Album(album_name: "Let Me Love You", album_author: "DJ Snake", album_cover: "p4"),
+    Album(album_name: "Shape Of You", album_author: "Ed Sherran", album_cover: "p5"),
+    Album(album_name: "Blank Space", album_author: "Taylor Swift", album_cover: "p6"),
+    Album(album_name: "Havana", album_author: "Camila Cabello", album_cover: "p7"),
+    Album(album_name: "Red", album_author: "Taylor Swift", album_cover: "p8"),
+    Album(album_name: "I Like It", album_author: "J Balvin", album_cover: "p9"),
+    Album(album_name: "Lover", album_author: "Taylor Swift", album_cover: "p10"),
+    Album(album_name: "7/27 Harmony", album_author: "Camila Cabello", album_cover: "p11"),
+    Album(album_name: "Joanne", album_author: "Lady Gaga", album_cover: "p12"),
+    Album(album_name: "Roar", album_author: "Kay Perry", album_cover: "p13"),
+    Album(album_name: "My Church", album_author: "Maren Morris", album_cover: "p14"),
+    Album(album_name: "Part Of Me", album_author: "Katy Perry", album_cover: "p15"),
+]
+
