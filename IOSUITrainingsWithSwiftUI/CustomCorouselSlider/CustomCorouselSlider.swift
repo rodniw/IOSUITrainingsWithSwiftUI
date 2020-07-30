7115d97404e5899b510fd49163d2a7ccc2cef172
Rodion <rodion.prosvirnin@bk.ru>
2020-07-30 23:15:11 +0300

    started custom slider creation.

---
+++ IOSUITrainingsWithSwiftUI/CustomCorouselSlider/CustomCorouselSlider.swift
@@ -0,0 +1,21 @@
+//
+//  CustomCorouselSlider.swift
+//  IOSUITrainingsWithSwiftUI
+//
+//  Created by Rodion Prosvirnin on 29.07.2020.
+//  Copyright © 2020 Rodion Prosvirnin. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomCorouselSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomCorouselSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomCorouselSlider()
+    }
+}
---
+++ IOSUITrainingsWithSwiftUI/CustomCorouselSlider/Story.swift
@@ -0,0 +1,18 @@
+//
+//  Story.swift
+//  IOSUITrainingsWithSwiftUI
+//
+//  Created by Rodion Prosvirnin on 30.07.2020.
+//  Copyright © 2020 Rodion Prosvirnin. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct Story : Identifiable {
+     
+     var id : Int
+     var image : String
+     var offset : CGFloat
+     var title : String
+ }
